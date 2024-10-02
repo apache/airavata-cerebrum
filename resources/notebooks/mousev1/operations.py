787a490528e545717e2b1df7a61c27ff03efa3ba
Sriram P C <sriram.pc@gmail.com>
2024-10-02 13:28:03 -0400

    added bkg noise for mouse v1

--- resources/notebooks/mousev1/operations.py
+++ resources/notebooks/mousev1/operations.py
@@ -4,6 +4,8 @@
 #
 import typing
 import numpy as np
+import h5py
+import pathlib
 from math import sqrt, exp, log
 from scipy.stats import multivariate_normal
 from scipy.special import erfinv
@@ -571,3 +573,117 @@ def select_bkg_sources(sources, target, n_syns, n_conn):
 
 def lgn_synaptic_weight_rule(source, target, base_weight, mean_size):
     return base_weight * mean_size / target["target_sizes"]
+
+
+
+def write_bkg(
+    output_filename, n_neu=1, duration=3.0, binsize=2.5e-4, rate=1000, seed=0
+):
+    # time units are seconds, (inverse: Hz)
+
+    nbins = int(duration / binsize)
+    np.random.seed(seed)
+    spike_bools = np.random.random([n_neu, nbins]) < (rate * binsize)
+    where = np.where(spike_bools)
+    # spikes_time = (np.where(spike_bools)[0] + 1) * binsize  # to avoid 0, still second
+    timestamps = (where[1] + 1) * binsize
+    # nids = np.zeros_like(spikes_time, dtype=np.uint)
+    nids = where[0]
+
+    # save
+    out_file = h5py.File(output_filename, "w")
+    # out_file["spikes/gids"] = nids
+    # add some random value to avoid the bad time stamps.
+    # out_file["spikes/timestamps"] = timestamps * 1000 + 0.01  # in ms
+    # let's use gzip level 6.
+    out_file.create_dataset(
+        "spikes/gids",
+        data=nids,
+        compression="gzip",
+        compression_opts=6,
+        shuffle=True,
+    )
+    out_file.create_dataset(
+        "spikes/timestamps",
+        data=timestamps * 1000 + 0.01,
+        compression="gzip",
+        compression_opts=6,
+        shuffle=True,
+    )
+    out_file.close()
+    return 0
+
+
+def write_regular_bkg(output_fpath: pathlib.Path, duration: float=1.0, interval: float=0.1) -> None:
+    spikes_time = np.linspace(interval, duration, int(duration / interval))
+    nids = np.zeros_like(spikes_time, dtype=np.uint)
+
+    # save
+    out_file = h5py.File(output_fpath, "w")
+    out_file["spikes/gids"] = nids
+    out_file["spikes/timestamps"] = spikes_time * 1000  # in ms
+    out_file.close()
+
+
+def generate_bkg_spikes(base_dir, bkg_file, bkg_dir="bkg"):
+    # try to write the bkg (let's make all of them)
+    # basedir = "small"
+    bkg_dir = pathlib.Path(base_dir, bkg_dir)
+    bkg_dir.mkdir(parents=True, exist_ok=True)
+    # read the bkg file and check the number of bkg units.
+    # bkg_file = f"{basedir}/network/bkg_nodes.h5"
+    # open the file and get the number of bkg units
+    with h5py.File(bkg_file, "r") as hfptr:
+        n_neu = hfptr["nodes"]["bkg"]["node_id"].shape[0]  # type: ignore
+
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_1kHz_100s.h5"
+    # write_bkg(bkg_name, n_neu, duration=100.0)
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_1kHz_10s.h5")
+    write_bkg(bkg_name, n_neu, duration=10.0)
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_1kHz_3s.h5")
+    write_bkg(bkg_name, n_neu)
+
+    # 250 Hz (new default for 100 background units)
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_250Hz_10s.h5")
+    write_bkg(bkg_name, n_neu, rate=250, duration=10.0)
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_250Hz_3s.h5")
+    write_bkg(bkg_name, n_neu, rate=250, duration=3.0)
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_100Hz_10s.h5")
+    # write_bkg(bkg_name, n_neu, rate=100, duration=10.0)
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_100Hz_3s.h5")
+    # write_bkg(bkg_name, n_neu, rate=100, duration=3.0)
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_2kHz_10s.h5")
+    # write_bkg(bkg_name, n_neu, rate=2000, duration=10.0)
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_2kHz_3s.h5")
+    # write_bkg(bkg_name, n_neu, rate=2000, duration=3.0)
+
+    # with 'full', all the bins are filled with spikes. i.e. no fluctuations.
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_full_100s.h5")
+    # write_bkg(bkg_name, n_neu, rate=4000, duration=100.0)
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_full_30s.h5")
+    # write_bkg(bkg_name, n_neu, rate=4000, duration=30.0)
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_full_10s.h5")
+    write_bkg(bkg_name, n_neu, rate=4000, duration=10.0)
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_full_3s.h5")
+    write_bkg(bkg_name, n_neu, rate=4000, duration=3.0)
+    # these require high resolution simulations
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_5kHz_3s.h5"
+    # write_bkg(bkg_name, n_neu, rate=5000, binsize=1.0e-5, duration=3.0)
+    # bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_5kHz_10s.h5"
+    # write_bkg(bkg_name, n_neu, rate=5000, binsize=1.0e-5, duration=10.0)
+
+    # regular bkg
+    bkg_name = pathlib.Path(bkg_dir, "bkg_spikes_regular_1s.h5")
+    write_regular_bkg(bkg_name)
+
+    # for the 8 direction stimuli (10 repetition)
+    start_seed = 381583
+    for i in range(8):
+        for j in range(10):
+            seed = start_seed + i * 10 + j
+            dirname = pathlib.Path(base_dir, "bkg_8dir_10trials", f"angle{i*45}_trial{j}")
+            pathlib.Path(dirname).mkdir(parents=True, exist_ok=True)
+            # write_bkg(pathlib.Path(dirname, "bkg_spikes_1kHz_3s.h5"), n_neu, seed=seed)
+            write_bkg(pathlib.Path(dirname, "bkg_spikes_250Hz_3s.h5"), n_neu, rate=250, seed=seed)
+            # write_bkg(pathlib.Path(dirname, "bkg_spikes_2kHz_3s.h5"), n_neu, rate=2000, seed=seed)
+            # write_bkg(pathlib.Path(dirname, "bkg_spikes_full_3s.h5"), n_neu, rate=4000, seed=seed)
